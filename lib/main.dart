147f52c8187f93ee314eebe9a7cd237bda2c6570
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-09-17 14:26:57 +0600

    Recent activity section work done

--- lib/main.dart
+++ lib/main.dart
@@ -91,12 +91,109 @@ class MyProfileApp extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 20.0), // Add spacing
-              ElevatedButton(
-                onPressed: () {
-                  // Add your button click action here
-                  print('Button Clicked!');
-                },
-                child: Text('Follow'),
+              Row(
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your button click action here
+                      // print('Button Clicked!');
+                    },
+                    child: const Text('Follow'),
+                  ),
+                  const SizedBox(width: 20.0), // Add spacing
+                  const Icon(
+                    Icons.message,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30.0), // Add spacing
+              const Text(
+                "Recent Activity",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+              const SizedBox(height: 10.0), // Add spacing
+              Container(
+                padding: const EdgeInsets.all(16.0), // Add padding
+                decoration: BoxDecoration(
+                  color: Colors.grey[500],
+                  borderRadius:
+                      BorderRadius.circular(10.0), // Add border radius
+                ),
+                child: Row(
+                  children: <Widget>[
+                    const Column(
+                      children: <Widget>[
+                        Text(
+                          "13/08/2023",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        Text(
+                          "A to Z instruction to visit Agra",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 20.0), // Add spacing
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.asset(
+                        'assets/download.jpeg',
+                        width: 80.0,
+                        height: 80.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.all(16.0), // Add padding
+                decoration: BoxDecoration(
+                  color: Colors.grey[500],
+                  borderRadius:
+                      BorderRadius.circular(10.0), // Add border radius
+                ),
+                child: Row(
+                  children: <Widget>[
+                    const Column(
+                      children: <Widget>[
+                        Text(
+                          "23/08/2023",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        Text(
+                          "Quick plan to visit Puri",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 20.0), // Add spacing
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.asset(
+                        'assets/download.jpeg',
+                        width: 80.0,
+                        height: 80.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
